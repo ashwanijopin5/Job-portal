bc1bd6343dfb6cd33c4b5290e22ad4d01e1342fe
Ashwani Kumar <ashwanijopin5@gmail.com>
2026-06-11 15:17:16 +0530

    frontend deployment

--- frontend/@/utils/constant.js
+++ frontend/@/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_END_POINT="http://localhost:8000/api/v1/user"
-export const job_END_POINT="http://localhost:8000/api/v1/job"
-export const APPLICATION_END_POINT="http://localhost:8000/api/v1/application"
-export const COMPANY_END_POINT="http://localhost:8000/api/v1/company"
+export const USER_END_POINT="https://job-portal-backend-ggps.onrender.com/api/v1/user"
+export const job_END_POINT="https://job-portal-backend-ggps.onrender.com/api/v1/job"
+export const APPLICATION_END_POINT="https://job-portal-backend-ggps.onrender.com/api/v1/application"
+export const COMPANY_END_POINT="https://job-portal-backend-ggps.onrender.com/api/v1/company"
